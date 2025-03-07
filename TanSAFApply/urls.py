326c46cf4e1f274c3aff8683a214e20e05b6f10d
Gift Christian <140011591+gcl140@users.noreply.github.com>
2025-03-07 14:37:58 +0300

    gigi

--- TanSAFApply/urls.py
+++ TanSAFApply/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("yuzzaz.urls")),
     path('apply/', include("applications.urls")),
+    path('accounts/login/', RedirectView.as_view(url='/login/', permanent=True)),
+
 ]
 
 # Serve media files in development (only works if DEBUG = True)
