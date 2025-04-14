94f6f6ca18a99bdaccdd43d42d215ce7ab0d10a8
Gift Christian <140011591+gcl140@users.noreply.github.com>
2025-04-14 18:47:04 +0300

    cool

--- yuzzaz/urls.py
+++ yuzzaz/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -10,4 +11,10 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('logout/', views.logout, name='logout'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='yuzzaz/password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='yuzzaz/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='yuzzaz/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name='yuzzaz/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
